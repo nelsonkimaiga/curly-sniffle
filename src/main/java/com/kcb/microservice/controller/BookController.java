ce5d4608d8a52786581787cf1d9b293b7294713e
Nelson Kimaiga <nelsonkimaiga@aol.com>
2025-11-12 11:18:47 +0300

    feat: Added swagger documentation

--- src/main/java/com/kcb/microservice/controller/BookController.java
+++ src/main/java/com/kcb/microservice/controller/BookController.java
@@ -2,6 +2,7 @@ package com.kcb.microservice.controller;
 
 import com.kcb.microservice.model.Book;
 import com.kcb.microservice.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,26 +27,31 @@ public class BookController {
 		this.bookService = bookService;
 	}
 	
+	@Operation(summary = "Creates a new book")
 	@PostMapping
 	public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(book));
 	}
 	
+	@Operation(summary = "Fetches all books")
 	@GetMapping
 	public ResponseEntity<List<Book>> getAllBooks() {
 		return ResponseEntity.status(HttpStatus.FOUND).body(bookService.findAllBooks());
 	}
 	
+	@Operation(summary = "Gets book details by Id")
 	@GetMapping("/{id}")
 	public ResponseEntity<Book> getBookDetailsById(@PathVariable Long id) {
 		return ResponseEntity.status(HttpStatus.FOUND).body(bookService.getBookDetailsById(id));
 	}
 	
+	@Operation(summary = "Updates a  book")
 	@PutMapping("/{id}")
 	public ResponseEntity<Book> updateBook(@PathVariable Long id, @Valid @RequestBody Book book) {
 		return ResponseEntity.status(HttpStatus.OK).body(bookService.updateBook(id, book));
 	}
 	
+	@Operation(summary = "Deletes a  book")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteBook(@PathVariable Long id) {
 		return ResponseEntity.status(HttpStatus.OK).body(bookService.deleteBook(id));
